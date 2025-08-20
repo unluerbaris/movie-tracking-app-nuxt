4707249d7a6f253c2d3a8c09763a8ea520b7489f
Baris Unluer <unluerbaris@gmail.com>
2025-08-20 15:10:29 +0300

    added dropdown functionality on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,9 +4,25 @@
       <div class="flex justify-between h-16">
         <div class="flex items-center space-x-8">
           <a href="/" class="text-2xl font-bold">CineTrack</a>
-          <div class="hidden md:flex space-x-6">
-            <a href="#" class="hover:text-gray-300">Movies</a>
-            <a href="#" class="hover:text-gray-300">TV Shows</a>
+          <div class="hidden md:flex space-x-6 relative">
+            <div class="relative group">
+              <a href="#" class="hover:text-gray-300 transition">Movies</a>
+              <div class="absolute text-sm w-38 bg-white text-black rounded-lg shadow-lg invisible group-hover:visible group-hover:opacity-100 duration-50" >
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Popular</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Now Playing</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Upcoming</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Top Rated</a>
+              </div>
+            </div>
+            <div class="relative group">
+              <a href="#" class="hover:text-gray-300 transition">TV Shows</a>
+              <div class="absolute text-sm w-38 bg-white text-black rounded-lg shadow-lg invisible group-hover:visible group-hover:opacity-100 duration-50" >
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Popular</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Airing Today</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">On TV</a>
+                <a href="#" class="block hover:rounded-lg px-4 py-2 hover:bg-gray-300">Top Rated</a>
+              </div>
+            </div>
             <a href="#" class="hover:text-gray-300">People</a>
             <a href="#" class="hover:text-gray-300">More</a>
           </div>
