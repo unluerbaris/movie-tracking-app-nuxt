f4b47b77a96eb44ba5a85a43fe3c7f977be12842
Baris Unluer <unluerbaris@gmail.com>
2025-08-29 23:23:45 +0300

    fixed progress bar position bug

--- components/Card.vue
+++ components/Card.vue
@@ -20,20 +20,26 @@
 </script>
 
 <template>
-  <div class="relative">
-    <div class="w-40 h-64 m-4 rounded-2xl shadow-lg overflow-hidden cursor-pointer hover:scale-105 duration-300 ">
-      <img 
-        :src="imageUrl" 
+  <div class="w-40 m-4 cursor-pointer hover:scale-105 duration-300">
+    <div class="relative w-full h-64 rounded-2xl shadow-lg">
+      <img
+        :src="imageUrl"
         :alt="title"
-        class="w-full h-64 object-cover"
+        class="w-full h-full object-cover rounded-2xl"
       />
+      <div class="absolute -bottom-5 left-3">
+        <ProgressBar :value="score" :size="40" />
+      </div>
     </div>
-    <div class="absolute bottom-32 left-6">
-      <ProgressBar :value="score" :size="36" />
-    </div>
-    <div class="p-4">
-      <h3 class="font-bold text-gray-800 cursor-pointer hover:text-blue-400 transition-colors duration-75">{{ title }}</h3>
-      <p class="text-sm text-gray-600 mt-1">{{ releaseDate }}</p>
+    <div class="mt-5 px-1">
+      <h3
+        class="font-bold text-gray-800 cursor-pointer hover:text-blue-400 transition-colors duration-75 line-clamp-2"
+      >
+        {{ title }}
+      </h3>
+      <p class="text-sm text-gray-600 mt-1">
+        {{ releaseDate }}
+      </p>
     </div>
   </div>
 </template>
